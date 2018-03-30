f99eef4489d545304ccbd40ec319cff0ef9e3ae4
Ralph Göstenmeier <rg@via-internet.de>
2018-03-30 18:33:53 +0200

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,61 +10,61 @@ import { LoginPage } from "../pages/login/login";
 import { LocalWeatherPage } from "../pages/local-weather/local-weather";
 
 export interface MenuItem {
-    title: string;
-    component: any;
-    icon: string;
+	title: string;
+	component: any;
+	icon: string;
 }
 
 @Component({
-  templateUrl: 'app.html'
+	templateUrl: 'app.html'
 })
 
-export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+export class MainApp {
+	@ViewChild(Nav) nav: Nav;
 
-  rootPage: any = LoginPage;
+	rootPage: any = LoginPage;
 
-  appMenuItems: Array<MenuItem>;
+	appMenuItems: Array<MenuItem>;
 
-  constructor(
-    public platform: Platform,
-    public statusBar: StatusBar,
-    public splashScreen: SplashScreen,
-    public keyboard: Keyboard
-  ) {
-    this.initializeApp();
+	constructor(
+		public platform: Platform,
+		public statusBar: StatusBar,
+		public splashScreen: SplashScreen,
+		public keyboard: Keyboard
+	) {
+		this.initializeApp();
 
-    this.appMenuItems = [
-      {title: 'Home', component: HomePage, icon: 'home'},
-      {title: 'Local Weather', component: LocalWeatherPage, icon: 'partly-sunny'}
-    ];
-  }
+		this.appMenuItems = [
+			{ title: 'Home', component: HomePage, icon: 'home' },
+			{ title: 'Local Weather', component: LocalWeatherPage, icon: 'partly-sunny' }
+		];
+	}
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
+	initializeApp() {
+		this.platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
 
-      //*** Control Splash Screen
-      // this.splashScreen.show();
-      // this.splashScreen.hide();
+			//*** Control Splash Screen
+			// this.splashScreen.show();
+			// this.splashScreen.hide();
 
-      //*** Control Status Bar
-      this.statusBar.styleDefault();
-      this.statusBar.overlaysWebView(false);
+			//*** Control Status Bar
+			this.statusBar.styleDefault();
+			this.statusBar.overlaysWebView(false);
 
-      //*** Control Keyboard
-      this.keyboard.disableScroll(true);
-    });
-  }
+			//*** Control Keyboard
+			this.keyboard.disableScroll(true);
+		});
+	}
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+	openPage(page) {
+		// Reset the content nav to have just this page
+		// we wouldn't want the back button to show in this scenario
+		this.nav.setRoot(page.component);
+	}
 
-  logout() {
-    this.nav.setRoot(LoginPage);
-  }
+	logout() {
+		this.nav.setRoot(LoginPage);
+	}
 
 }
